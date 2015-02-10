82fe81920b1f8e287b8f514668592ccf9b7d75ba
System Administrator <root@Qdladoooo.local>
2015-02-10 14:49:36 +0800

    sync

--- cm_crawller/r.pl
+++ cm_crawller/r.pl
@@ -4,7 +4,7 @@ use utils;
 use URI::Escape;
 
 my $dbh = utils->get_dbh();
-my $sql = "select name, district, address, lng, lat, postcode, city from map2 where lng is not null and address is not null";
+my $sql = "select distinct name, district, address, lng, lat, postcode, city from map_final where lng is not null and address is not null and address like '上海市%' order by id asc";
 my $sth = $dbh->prepare($sql);
 $sth->execute();
 
@@ -13,6 +13,7 @@ open OP1, '>./data/cm_info.csv';
 while (my @row = $sth->fetchrow_array()) {
     my $line = join('", "', @row);
     print OP1 "\"$line\"\n";
+    #print $line . "\n";
 }
 
 $sth->finish();
